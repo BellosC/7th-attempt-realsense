aad3bff08972887100ddb07c33adf7b046da30a1
Costas Bellos <costas.bellos@hotmail.com>
2020-09-15 09:11:19 +0300

    7th-attempt-Realsense
    tests

---
+++ Coding-Attempts-Bellos/test_14_09/no SDK_attempt_1_Face_Recognition.py
@@ -0,0 +1,36 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if key "q" is pressed
+    key = cv2.waitKey(1)
+    # Press 'q' to close the image window
+    if key & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+# I added this line to check something in Git - not important
+# Release the VideoCapture object
+cap.release()
