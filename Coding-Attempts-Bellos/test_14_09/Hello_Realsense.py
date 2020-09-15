41fafcae28c41ed1874676a845084007e472545f
BellosC <70270581+BellosC@users.noreply.github.com>
2020-09-15 12:22:24 +0300

    Update Hello_Realsense.py

--- Coding-Attempts-Bellos/test_14_09/Hello_Realsense.py
+++ Coding-Attempts-Bellos/test_14_09/Hello_Realsense.py
@@ -2,40 +2,40 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-# Configure depth and color streams
+# Διαμορφωσε τις ροές βάθους και χρώματος
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start streaming
+# Άρχισε το  streaming
 pipeline.start(config)
 
 try:
     while True:
 
-        # Wait for a coherent pair of frames: depth and color
+        # Περιμενε για το ζεύγος πλαισίων: βάθος και χρώμα
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
         if not depth_frame or not color_frame:
             continue
 
-        # Convert images to numpy arrays
+        # Μετέτρεψε εικόνες σε numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        # Εφάρμοσε colormap στην depth image 
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-        # Stack both images horizontally
+        # Τοποθέτησε και τις 2 εικόνες οριζόντια
         images = np.hstack((color_image, depth_colormap))
 
-        # Show images
+        # Εμφάνησε τις εικόνες
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
         key = cv2.waitKey(1)
-        # Press esc or 'q' to close the image window
+        # Πάτα το 'q' για να κλείσει το παράθυρο αφου πρώτα το έχεις επιλέξει
         if key & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
@@ -43,5 +43,5 @@ try:
 
 finally:
 
-    # Stop streaming
+    # Διέκοψε τη ροή
     pipeline.stop()
